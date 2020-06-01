66e7acdd014d080bf7462fc60a103b7cf022a597
TaeHun Kang <taehkang@amazon.com>
2020-06-01 16:28:26 -0700

    Reformatting and implementing suggestions

--- src/cfnlint/customRules/Operators.py
+++ src/cfnlint/customRules/Operators.py
@@ -1,26 +1,13 @@
-"""
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: MIT-0
-"""
+import cfnlint.custom_rules
+
 
 def equalsOp(rule, propertyList):
     """ Process EQUALS operators """
-    if not propertyList:
-        return 'Error - Invalid Resource Type ' + rule.resourceType
+    if len(propertyList) == 0:
+        return 'Error - Invalid Resource Type ' + rule[0]
     for prop in propertyList:
-        actualValue = getProperty(prop, rule.prop)
-        if actualValue.strip() != str(rule.value).strip():
-            return 'Not Equal as ' + actualValue + ' does not equal ' + rule.value
+        actualValue = cfnlint.custom_rules.getProperty(prop, rule[1])
+        if actualValue.strip() != str(rule[3]).strip():
+            return 'Not Equal as ' + actualValue + ' does not equal ' + rule[3]
     return 'EQUALS'
 
-def getProperty(json, nestedProperties):
-    """ Converts dot format strings to resultant values -
-    i.e inputting 'Value.InstanceSize' to nestedProperties will output the value of that specific property from json"""
-    nestedProperties = 'Value.' + str(nestedProperties)
-    properties = nestedProperties.split('.')
-    for prop in properties:
-        try:
-            json = json[prop]
-        except KeyError as e:
-            return 'The following property was not found - "%s"' % str(e)
-    return str(json)
